7bada00f74d4e9347a7ad6fe38ef5a3bdb799b96
NexVRStudios <99061870+NexVRStudios@users.noreply.github.com>
2022-09-27 21:42:04 -0400

    Add project files.

---
+++ DLLTest/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLTest {
+    public class HelloWorld {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            //closes the debugger on a key press
+            Close();
+        }
+
+        private static void Close()
+        {
+            Console.WriteLine("\nPress Any Key To Close");
+            Console.ReadKey();
+        }
+    }
+}
